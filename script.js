723155d7c4c03e73189e3b02394af89f66daf3a8
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 15:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  let result = [];
-  let currentSubarray = [];
-
-  for (const num of arr) {
-    // Check if adding the current number exceeds the maximum sum
-    if (currentSubarray.reduce((sum, value) => sum + value, 0) + num <= n) {
-      currentSubarray.push(num);
-    } else {
-      // Start a new subarray if adding the current number exceeds the maximum sum
-      result.push(currentSubarray);
-      currentSubarray = [num];
-    }
-  }
-
-  // Add the last subarray
-  if (currentSubarray.length > 0) {
-    result.push(currentSubarray);
-  }
-
-  return result;
+  // Write your code here
+	let subarray = [],ans=[],maxsum=0;
+	for(let i=0;i<arr.length;i++){
+		if(maxsum+arr[i] <= n){
+			maxsum+=arr[i];
+			subarray.push(arr[i]);
+		}
+		else{
+			ans.push(subarray);
+			maxsum=arr[i];
+			subarray[arr[i]];
+		}
+	}
+	return ans;
 };
-
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, parseInt(n))));
